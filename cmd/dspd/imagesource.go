9f7be0d65dba9e684723ea7c239259430ad8975e
Pietro Lorefice <pietro.lorefice@gmail.com>
2017-05-02 15:28:03 +0200

    dspd: add raw image source

--- cmd/dspd/imagesource.go
+++ cmd/dspd/imagesource.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/base64"
+	"encoding/binary"
 	"encoding/gob"
 	"image"
 	_ "image/jpeg"
@@ -46,6 +48,34 @@ func NewBase64ImageSource(r io.Reader) *Base64ImageSource {
 
 func (bis *Base64ImageSource) Read() (image.Image, error) {
 	m, _, err := image.Decode(bis.dec)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+type RawImageSource struct {
+	r io.Reader
+}
+
+func NewRawImageSource(r io.Reader) *RawImageSource {
+	return &RawImageSource{
+		r: r,
+	}
+}
+
+func (ris *RawImageSource) Read() (image.Image, error) {
+	var size uint64
+	if err := binary.Read(ris.r, binary.LittleEndian, &size); err != nil {
+		return nil, err
+	}
+
+	data := make([]byte, size)
+	if _, err := io.ReadFull(ris.r, data); err != nil {
+		return nil, err
+	}
+
+	m, _, err := image.Decode(bytes.NewReader(data))
 	if err != nil {
 		return nil, err
 	}
