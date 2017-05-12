7d1488d0969e6d32b94313c3dd43e1c20d7519f1
Pietro Lorefice <pietro@develer.com>
2017-05-12 09:47:35 +0200

    bot: support for textd functionalities

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -1,25 +1,55 @@
 package main
 
 import (
+	"flag"
 	"os"
 
+	log "github.com/Sirupsen/logrus"
+	"github.com/develed/develed/config"
+	srv "github.com/develed/develed/services"
 	"github.com/develed/develed/slackbot"
 	"github.com/nlopes/slack"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var (
+	cfg = flag.String("config", "/etc/develed.toml", "configuration file")
 )
 
 func main() {
+	flag.Parse()
+
+	conf, err := config.Load(*cfg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	bot := slackbot.New(os.Getenv("SLACK_BOT_TOKEN"))
+
+	conn, err := grpc.Dial(conf.Textd.GRPCServerAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+
+	textd := srv.NewTextdClient(conn)
 
 	bot.DefaultResponse(func(b *slackbot.Bot, msg *slack.Msg) {
 		bot.Message(msg.Channel, "Non ho capito")
 	})
 
-	bot.RespondTo("ciao", func(b *slackbot.Bot, msg *slack.Msg, args ...string) {
-		bot.Message(msg.Channel, "Olà!")
-	})
+	bot.RespondTo("scrivi (.*)", func(b *slackbot.Bot, msg *slack.Msg, args ...string) {
+		text := args[1]
 
-	bot.RespondTo("echo (.*)", func(b *slackbot.Bot, msg *slack.Msg, args ...string) {
-		bot.Message(msg.Channel, "Hai scritto: "+args[1])
+		_, err := textd.Write(context.Background(), &srv.TextRequest{
+			Text: text,
+		})
+		if err != nil {
+			log.Errorln(err)
+		} else {
+			bot.Message(msg.Channel, "Hai scritto: "+text)
+		}
 	})
 
 	bot.Start()
