4674140819da4299aecf7ae3a0e6f4c01a866d75
Daniele Basile <asterix24@gmail.com>
2017-06-04 17:53:18 +0200

    bitmapfont: Use configuration module for all fonts. Clean up code. Open font file in init.

--- bitmapfont/bitmapfont.go
+++ bitmapfont/bitmapfont.go
@@ -1,61 +1,24 @@
 package bitmapfont
 
 import (
+	"errors"
 	"image"
 	"image/color"
 	"os"
 
 	log "github.com/Sirupsen/logrus"
+	conf "github.com/develed/develed/config"
 )
 
-type FontMgr struct {
-	img   image.Image
-	width int
-	high  int
-	col   int
-	row   int
-}
+var fontImageTable image.Image
+var Config conf.BitmapFont
 
-type FontInterface interface {
-	Render(fontPath string, fontName string, text string) (image.Image, error)
-}
+func Render(text string, char_space int, top_off int) (image.Image, error) {
 
-var cTable = map[string][]int{
-	// Font     name,   high width rows, columns
-	"font5x7": {7, 5, 4, 24},
-	//"font4x7": {4, 7},
-	//"font6x6": {6, 6},
-	//"font6x8": {6, 8},
-	//"font6x7": {6, 7},
-}
+	img_rect := fontImageTable.Bounds()
+	fontcolums := img_rect.Dx() / Config.Width
 
-func (f *FontMgr) Render(fontPath string, fontName string, text string, char_space int, top_off int) (image.Image, error) {
-
-	if fontName == "" {
-		fontName = "font5x7"
-	}
-
-	reader, err := os.Open(fontPath + string(os.PathSeparator) + fontName + ".png")
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-	defer reader.Close()
-
-	// Decode fonts table.
-	cImg, _, err := image.Decode(reader)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-
-	f.img = cImg
-	f.high = cTable[fontName][0]
-	f.width = cTable[fontName][1]
-	f.row = cTable[fontName][2]
-	f.col = cTable[fontName][3]
-
-	frame_width := len(text)*f.width + (len(text)-1)*(char_space)
+	frame_width := len(text)*Config.Width + (len(text)-1)*(char_space)
 	log.Debug("len ", frame_width)
 
 	// Allocate frame
@@ -71,23 +34,51 @@ func (f *FontMgr) Render(fontPath string, fontName string, text string, char_spa
 
 	// Fill frame
 	for n, key := range text {
-		col := int(key-' ') % f.col
-		row := int(key-' ') / f.col
+		col := int(key-' ') % fontcolums
+		row := int(key-' ') / fontcolums
 
 		log.Debug("offset ", int(key-' '))
 		log.Debug("Col ", col)
 		log.Debug("Row ", row)
 
-		for y := 0; y < f.high+top_off; y++ {
-			for x := 0; x < f.width+char_space; x++ {
-				if x >= f.width {
-					img.Set(x+n*(f.width+char_space), y+top_off, color.RGBA{0, 0, 0, 255})
+		for y := 0; y < Config.High+top_off; y++ {
+			for x := 0; x < Config.Width+char_space; x++ {
+				if x >= Config.Width {
+					img.Set(x+n*(Config.Width+char_space), y+top_off, color.RGBA{0, 0, 0, 255})
 				} else {
-					img.Set(x+n*(f.width+char_space), y+top_off, f.img.At(x+f.width*col, y+f.high*row))
+					img.Set(x+n*(Config.Width+char_space), y+top_off, fontImageTable.At(x+Config.Width*col, y+Config.High*row))
 				}
 			}
 		}
 	}
 
 	return img, nil
+}
+
+func Init(path string, name string, cfg []conf.BitmapFont) error {
+	if name == "" {
+		name = "font5x7"
+	}
+
+	for _, s := range cfg {
+		log.Debug("Cerco font ", s.Name, " ", name)
+		if name == s.Name {
+			Config = s
+			log.Debug(path + string(os.PathSeparator) + Config.FileName)
+			reader, err := os.Open(path + string(os.PathSeparator) + Config.FileName)
+			if err != nil {
+				return err
+			}
+			defer reader.Close()
+
+			// Decode fonts table.
+			fontImageTable, _, err = image.Decode(reader)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+
+	return errors.New("Wrong BitmatFont name.\n")
 }
