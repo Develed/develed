0c5bec1683283e3b1ac0e98d9fa4c7f00532e7a6
Matteo Pieraccioni <mpieraccioni@gmail.com>
2017-09-18 10:41:37 +0200

    dspd:modified due to allow use of priority queue

--- cmd/dspd/termsink.go
+++ cmd/dspd/termsink.go
@@ -8,6 +8,9 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"os"
+	"time"
+
+	queue "github.com/develed/develed/queue"
 
 	"github.com/develed/develed/imconv"
 	srv "github.com/develed/develed/services"
@@ -19,9 +22,14 @@ import (
 // It requires a Truecolor-capable terminal in order to render images correctly.
 type TermSink struct{}
 
+var q queue.Queue
+
 // NewTermSink creates a new TermSink. It returns an error if the sdout of the
 // calling process is not a terminal.
+
 func NewTermSink() (*TermSink, error) {
+	//goroutine passo coda
+	go DrawRoutine(&q)
 	if !terminal.IsTerminal(int(os.Stdout.Fd())) {
 		return nil, errors.New("TermSink requires stdout to be a terminal")
 	}
@@ -33,24 +41,37 @@ func (ts *TermSink) Run() error {
 }
 
 func (ts *TermSink) Draw(ctx context.Context, req *srv.DrawRequest) (*srv.DrawResponse, error) {
-	img, _, err := image.Decode(bytes.NewReader(req.Data))
-	if err != nil {
-		return nil, err
-	}
-	fmt.Print("\033[s")
-	fmt.Print("\n\n")
-	sz := img.Bounds().Size()
-	for y := 0; y < sz.Y; y++ {
-		for x := 0; x < sz.X; x++ {
-			col := imconv.NormalizeColor(img.At(x, y))
-			if _, err := fmt.Printf("\033[48;2;%d;%d;%dm ", col.R, col.G, col.B); err != nil {
-				return nil, err
-			}
-		}
-		if _, err := fmt.Print("\033[0m\n"); err != nil {
-			return nil, err
-		}
-	}
-	fmt.Print("\033[u")
+	n := queue.Node{req.Priority, req.Timeslot, req.Data}
+	q.Push(&n)
+	fmt.Println(n.Priority)
 	return &srv.DrawResponse{Code: 0, Status: "OK"}, nil
+}
+
+func DrawRoutine(q *queue.Queue) {
+	for {
+		node := q.Pop()
+		if node != nil {
+			fmt.Println(int64(node.Priority))
+			img, _, err := image.Decode(bytes.NewReader(node.Data))
+			if err != nil {
+				continue
+			}
+			fmt.Print("\033[s")
+			fmt.Print("\n\n")
+			sz := img.Bounds().Size()
+			for y := 0; y < sz.Y; y++ {
+				for x := 0; x < sz.X; x++ {
+					col := imconv.NormalizeColor(img.At(x, y))
+					if _, err := fmt.Printf("\033[48;2;%d;%d;%dm ", col.R, col.G, col.B); err != nil {
+						continue
+					}
+				}
+				if _, err := fmt.Print("\033[0m\n"); err != nil {
+					continue
+				}
+			}
+			fmt.Print("\033[u")
+			time.Sleep(time.Duration(node.TimeSlot) * time.Millisecond)
+		}
+	}
 }
